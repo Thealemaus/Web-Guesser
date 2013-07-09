9eda57e399874f7f4c8e81c80204b628b0727971
Alejandro Laserna <alejandro.laserna963@gmail.com>
2013-07-09 16:04:32 -0400

    Added if then statement to index file

--- web_guesser_basic.rb
+++ web_guesser_basic.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
+@user_guess = ""
 
-
-number = (1 + rand(99)).to_s
+number = (1 + rand(99))
 
 get "/" do
+	@user_guess = params["guess"].to_i
 	erb :index, :locals => {:number => number}
 end
