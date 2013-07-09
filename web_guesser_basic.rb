9128a63a8a943519d54dbd8db70c4d6f3292237e
Alejandro Laserna <alejandro.laserna963@gmail.com>
2013-07-09 14:54:01 -0400

    Created the folder views and ERB template with some HTML

--- web_guesser_basic.rb
+++ web_guesser_basic.rb
@@ -2,9 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-secret_number = x = (1 + rand(99)).to_s
+number = (1 + rand(99)).to_s
 
 get "/" do
-	
-	"the secret number is #{secret_number}"
+	erb :index, :locals => {:number => number}
 end
